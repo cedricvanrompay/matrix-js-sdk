04775bca7b90b400827892c8492b75e4b826c8c7
Cédric Van Rompay <cedric.vanrompay@misakey.com>
2020-01-15 17:25:30 +0100

    pass deviceKey and sessionId to callback in getAllEndToEndSessions

--- src/crypto/store/memory-crypto-store.js
+++ src/crypto/store/memory-crypto-store.js
@@ -259,11 +259,15 @@ export default class MemoryCryptoStore {
     }
 
     getAllEndToEndSessions(txn, func) {
-        for (const deviceSessions of Object.values(this._sessions)) {
-            for (const sess of Object.values(deviceSessions)) {
-                func(sess);
-            }
-        }
+        Object.entries(this._sessions).forEach(([deviceKey, deviceSessions]) => {
+            Object.entries(deviceSessions).forEach(([sessionId, session]) => {
+                func({
+                    ...session,
+                    deviceKey,
+                    sessionId,
+                });
+            });
+        });
     }
 
     storeEndToEndSession(deviceKey, sessionId, sessionInfo, txn) {
